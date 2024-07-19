6955a935b09d6a212ed53cf16ee308d77d9e92ec
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-07-19 19:12:44 +0530

    User Registeration

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import MyTokenObtainPairView
+from .views import MyTokenObtainPairView, register_user
 
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', views.getRoutes),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', register_user, name = 'register_user'),
 ]
--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -2,6 +2,9 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view as view
 
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http import HttpResponse
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -28,3 +31,23 @@ def getRoutes(request):
     ]
 
     return Response(routes)
+
+# Create your views here.
+def register_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = User.objects.filter(username = username)
+
+        if user.exists():
+            messages.info(request, "User already exists")
+            return HttpResponse.status_code(409, "User already exists")
+
+        user = User.objects.create_user(username = username, password = password)        
+        user.save()
+
+        messages.info(request, "User created successfully")
+        return HttpResponse.status_code(200, "User created successfully")
+        
+    return HttpResponse.status_code(400, "Bad request")
